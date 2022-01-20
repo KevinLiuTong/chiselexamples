79395628f19ed2d8be600da7e3e58d5ee9be70eb
KevinLiuTong <kevinliutong@hotmail.com>
2022-01-20 10:10:46 +0800

    init commit

---
+++ main/src/scala/basics.scala
@@ -0,0 +1,14 @@
+package chiselexamples
+
+import chisel3._
+import chsiel3.util._
+
+class MyModule extends RawModule{
+
+  val io = IO(new Bundle{
+    val in = Input(UInt(3.W))
+    val out = Output(UInt(3.W))
+  })
+
+  io.out := io.in + 1.U
+}
